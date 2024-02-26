751ab60a7461efdfaed86c03945436f6f8034e98
Savvietarmac Wendel <eric.wendelsal@gmail.com>
2024-02-26 14:54:49 +0000

    add colorama library and started adding functions to run.py

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import colorama
+colorama.init()
+
+def main_menu():
+    print(colorama.fore.BLUE + colorama.back.GREEN "Welcome to Simonsays!\n")
+    print(colorama.fore.YELLOW + colorama.back.Red "Now with Color!")
+    print("")
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    main_menu()
+
+main()
