61a6b4a964f8e10a9cc1b5adc4c6881428504418
Savvietarmac Wendel <eric.wendelsal@gmail.com>
2024-02-27 17:27:45 +0000

    add color functions to run.py

--- run.py
+++ run.py
@@ -1,16 +1,31 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import colorama
 colorama.init()
-
+"""
 def main_menu():
-    print(colorama.fore.BLUE + colorama.back.GREEN "Welcome to Simonsays!\n")
-    print(colorama.fore.YELLOW + colorama.back.Red "Now with Color!")
-    print("")
+    
+    Main menu for the game, used to send the user further.
+    
+    print(colorama.Fore.BLUE + colorama.Back.YELLOW + "Welcome to Simonsays!\n")
+    print(colorama.Fore.YELLOW + colorama.Back.YELLOW + "Now with Color!\n")
+    print(colorama.Fore.CYAN + colorama.Back.WHITE + "Press 'A' to start the game\n")
+"""    
+def green_color_square():
+    print(colorama.Back.GREEN + "   \n")
+    return green_point
+    
 
+def blue_color_square():
+    print(colorama.Back.BLUE + "   \n")
+    return blue_point
 
+def red_color_square():
+    print(colorama.Back.RED + "   \n")
+    return red_point
 
-
-
+def yellow_color_square():
+    print(colorama.Back.YELLOW + "   \n")
+    return yellow_point
 
 
 
@@ -19,6 +34,8 @@ def main_menu():
 
 
 def main():
-    main_menu()
+    #main_menu()
+    green_color_square()
+    blue_color_square()
 
 main()
