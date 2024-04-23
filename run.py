f36dd6335d72e991c0ce3d86f9cbb503b2e27502
Savvietarmac Wendel <eric.wendelsal@gmail.com>
2024-04-23 07:49:54 +0000

    move display_welcome functon

--- run.py
+++ run.py
@@ -4,21 +4,6 @@ import colorama
 import randome
 
 colorama.init()
-"""
-def main_menu():
-    
-    Main menu for the game, used to send the user further.
-    
-    print(colorama.Fore.BLUE + colorama.Back.YELLOW + "Welcome to Simonsays!\n")
-    print(colorama.Fore.YELLOW + colorama.Back.YELLOW + "Now with Color!\n")
-    print(colorama.Fore.CYAN + colorama.Back.WHITE + "Press 'A' to start the game\n")
-"""    
-def display_welcome():
-    print("Welcome to Simon Says\n")
-    print("Use the first letter in each color to guess the order.\n")
-    print(colorama.Fore.GREEN + "G " + colorama.Fore.BLUE + "B " + colorama.Fore.RED + "R " + colorama.Fore.YELLOW + "Y ")
-    print("choose a number between 3 and 30.\n")
-
 
 #The desired amount of times the game plays
 desired_amount = 0
@@ -27,18 +12,18 @@ desired_amount = 0
 #points added for everytime a colored box apperesss
 points = 0
 
-"""
-All color funtions underneath, add points to point decleration,
-also a delay for each time one activates,
-prints the colored boxes in the terminal.
-"""
-
 colours = {
     "G": colorama.back.GREEN + " " + colorama.Style.RESET_ALL,
     "B": colorama.back.BLUE + " " + colorama.Style.RESET_ALL,
     "R": colorama.back.RED + " " + colorama.Style.RESET_ALL,
     "Y": colorama.back.YELLOW + " " + colorama.Style.RESET_ALL,
 }
+
+def display_welcome():
+    print("Welcome to Simon Says\n")
+    print("Use the first letter in each color to guess the order.\n")
+    print(colorama.Fore.GREEN + "G " + colorama.Fore.BLUE + "B " + colorama.Fore.RED + "R " + colorama.Fore.YELLOW + "Y ")
+    print("choose a number between 3 and 30.\n")
 
 def green_color_square():
     global points
